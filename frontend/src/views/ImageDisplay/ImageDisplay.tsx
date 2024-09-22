8e894b322094e1741a446d0691c842db2d9e9ce2
ZHANG HERAN <zhang-heran@rgsis.com>
2024-09-22 23:12:31 +0900

    feature: add image popup feature.

--- frontend/src/views/ImageDisplay/ImageDisplay.tsx
+++ frontend/src/views/ImageDisplay/ImageDisplay.tsx
@@ -1,21 +1,51 @@
-import { useSelector } from "react-redux";
-import { RootState } from "../../store/store";
+import { useState } from 'react';
+import { useSelector } from 'react-redux';
+import { RootState } from '../../store/store';
 
 const ImageDisplay = () => {
-
   const progress = useSelector((state: RootState) => state.music.progress);
   const imageBase64 = useSelector((state: RootState) => state.music.base64image);
   const showText = useSelector((state: RootState) => state.music.showText);
+  const [showPopup, setShowPopup] = useState(false);
 
-  if (progress == 5 && !showText) {
+  const togglePopup = () => {
+    setShowPopup(!showPopup);
+  };
+
+  if (progress === 5 && !showText) {
     return (
-      <img src={`data:image/jpeg;base64,${imageBase64}`} alt="Music" style={{maxHeight: '50vh'}}/>
+      <>
+        <img
+          src={`data:image/jpeg;base64,${imageBase64}`}
+          alt="Music"
+          style={{ maxHeight: '50vh', maxWidth: '100%' }}
+          onClick={togglePopup}
+        />
+        {showPopup && (
+          <div style={{
+            position: 'fixed',
+            top: 0,
+            left: 0,
+            width: '100%',
+            height: '100%',
+            backgroundColor: 'rgba(0, 0, 0, 0.8)',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            zIndex: 1000
+          }} onClick={togglePopup}>
+            <img
+              src={`data:image/jpeg;base64,${imageBase64}`}
+              alt="Music"
+              style={{ maxHeight: '90vh', maxWidth: '90%' }}
+            />
+          </div>
+        )}
+      </>
     );
   }
 
-  return (
-    <></>
-  );
+  return <></>;
 };
 
 export default ImageDisplay;
